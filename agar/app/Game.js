7439de2a081b23f7b76e2da4df8ff7f388f02574
akupiec <akupiec88@gmail.com>
2016-07-15 19:27:39 +0200

    map generator

--- agar/app/Game.js
+++ agar/app/Game.js
@@ -1,15 +1,68 @@
 import Player from "./Player";
+import MapGenerator from './MapGenerator';
 import Phaser from 'Phaser';
+
+const TITLE_SIZE = 32;
+const WIDTH = 1000;
+const HEIGHT = 1000;
+
+const MAP_DATA = {
+    "layers": [
+        {
+            data: null,
+            "name": "World1",
+            "opacity": 1,
+            "properties": {
+                "time": "1000"
+            },
+            "type": "tilelayer",
+            "visible": true,
+            "height": HEIGHT,
+            "width": WIDTH,
+            "x": 0,
+            "y": 0
+        }],
+    "orientation": "orthogonal",
+    "properties": {
+        "mapProp": "123"
+    },
+    "tileheight": TITLE_SIZE,
+    "tilewidth": TITLE_SIZE,
+    "tilesets": [
+        {
+            "firstgid": 0,
+            "image": "Dungeon_A2.png",
+            "imageheight": 384,
+            "imagewidth": 512,
+            "margin": 0,
+            "name": "SuperMarioBros-World1-1",
+            "properties": {},
+            "spacing": 0,
+            "tileproperties": {
+                "10": {
+                    "coin": "true"
+                }
+            },
+            "tileheight": TITLE_SIZE,
+            "tilewidth": TITLE_SIZE
+        }],
+    "version": 1,
+    "height": HEIGHT,
+    "width": WIDTH
+};
 
 export default class Game {
     constructor() {
         this._game = null;
         this._cursors = null;
         this._player = null;
+        this._generator = new MapGenerator();
     }
 
     run() {
-        this._game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', {
+        this._generator.generate();
+
+        this._game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
             preload: this._preload.bind(this),
             create: this._create.bind(this),
             update: this._update.bind(this),
@@ -17,19 +70,36 @@ export default class Game {
         });
     }
 
+
     _preload() {
         this._game.stage.backgroundColor = '#007236';
 
-        this._game.load.image('mushroom', 'assets/sprites/mushroom2.png');
-        this._game.load.image('phaser', 'assets/sprites/sonic_havok_sanity.png');
+        // this._game.load.image('mushroom', 'assets/sprites/mushroom2.png');
+        // this._game.load.image('phaser', 'assets/sprites/sonic_havok_sanity.png');
+
+        const DATA = this._generator.getMap();
+
+        MAP_DATA.layers[0].data = DATA;
+        this._game.load.tilemap('mario', null, MAP_DATA, Phaser.Tilemap.TILED_JSON);
+        this._game.load.image('tiles', 'dist/assets/sprites/Dungeon_A2.png');
     }
 
-    _create() {
-        this._game.world.resize(1200, 800);
 
-        for (var i = 0; i < 100; i++) {
-            this._game.add.sprite(this._game.world.randomX, this._game.world.randomY, 'mushroom');
-        }
+    _create() {
+        this._game.world.resize(32000, 32000);
+
+        var titleMap = this._game.add.tilemap('mario');
+        titleMap.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
+        var layer = titleMap.createLayer('World1');
+
+        layer.resizeWorld();
+
+        // this._game.add.tileSprite(0, 0,500, 500, 'terrain', 1);
+
+        // for (var i = 0; i < 100; i++) {
+        //     this._game.add.sprite(this._game.world.randomX, this._game.world.randomY, 'terrain', 0);
+        //     this._game.add.sprite(this._game.world.randomX, this._game.world.randomY, 'mushroom');
+        // }
         this._cursors = this._game.input.keyboard.createCursorKeys();
 
         this._player = new Player('phaser', 5, this._game);
--- agar/app/mapGenerator.js
+++ agar/app/mapGenerator.js
@@ -1,4 +1,4 @@
-class MapGenerator {
+export default class MapGenerator {
     generate() {
         console.time('generate');
 
@@ -31,43 +31,117 @@ class MapGenerator {
             lava: voronoiMap.lava()
         };
 
-//         switch (shape) {
-//             case 'blob' :
-//                 state.map.newIsland(voronoiMap.islandShape.makeBlob(), seed);
-//                 break;
-//             case 'noise' :
-//                 state.map.newIsland(voronoiMap.islandShape.makeNoise(shapeSeed), seed);
-//                 break;
-//             case 'perlin' :
-//                 state.map.newIsland(voronoiMap.islandShape.makePerlin(shapeSeed, oceanRatio), seed);
-//                 break;
-//             case 'radial' :
-//                 state.map.newIsland(voronoiMap.islandShape.makeRadial(shapeSeed, islandFactor), seed);
-//                 break;
-//             case 'square' :
-//                 state.map.newIsland(voronoiMap.islandShape.makeSquare(), seed);
-//                 break;
-//         }
-//
-//
-//         const ps = voronoiMap.pointSelector.generateRelaxed(size.width, size.height, seed, lloydIterations);
-//
-//         state.map.go0PlacePoints(numberOfPoints, ps);
-//         state.map.go1BuildGraph();
-//         state.map.go2AssignElevations(lakeThreshold);
-//         state.map.go3AssignMoisture(riverChance);
-//         state.map.go4DecorateMap();
-//
-//         state.roads.createRoads(state.map, roadElevations);
-// // state.lava.createLava(state.map, 0.8);
-//         state.watersheds.createWatersheds(state.map);
-//         state.noisyEdges.buildNoisyEdges(state.map, state.lava, seed, edgeNoise);
+        switch (shape) {
+            case 'blob' :
+                state.map.newIsland(voronoiMap.islandShape.makeBlob(), seed);
+                break;
+            case 'noise' :
+                state.map.newIsland(voronoiMap.islandShape.makeNoise(shapeSeed), seed);
+                break;
+            case 'perlin' :
+                state.map.newIsland(voronoiMap.islandShape.makePerlin(shapeSeed, oceanRatio), seed);
+                break;
+            case 'radial' :
+                state.map.newIsland(voronoiMap.islandShape.makeRadial(shapeSeed, islandFactor), seed);
+                break;
+            case 'square' :
+                state.map.newIsland(voronoiMap.islandShape.makeSquare(), seed);
+                break;
+        }
+
+
+        const ps = voronoiMap.pointSelector.generateRelaxed(size.width, size.height, seed, lloydIterations);
+
+        state.map.go0PlacePoints(numberOfPoints, ps);
+        state.map.go1BuildGraph();
+        state.map.go2AssignElevations(lakeThreshold);
+        state.map.go3AssignMoisture(riverChance);
+        state.map.go4DecorateMap();
+
+        state.roads.createRoads(state.map, roadElevations);
+// state.lava.createLava(state.map, 0.8);
+        state.watersheds.createWatersheds(state.map);
+        state.noisyEdges.buildNoisyEdges(state.map, state.lava, seed, edgeNoise);
         console.timeEnd('generate');
+
         this._map = state.map;
     }
 
+    mapBiomeToSpriteNr(biome) {
+        switch (biome) {
+            case 'OCEAN':
+                return 0;
+            case 'MARSH':
+                return 2;
+            case 'ICE':
+                return 4;
+            case 'LAKE':
+                return 6;
+            case 'BEACH':
+                return 8;
+            case 'SNOW':
+                return 10;
+            case 'TUNDRA':
+                return 11;
+            case 'BARE':
+                return 12;
+            case 'SCORCHED':
+                return 13;
+            case 'TAIGA':
+                return 15;
+            case 'SHRUBLAND':
+                return 0;
+            case 'TEMPERATE_DESERT':
+                return 0;
+            case 'TEMPERATE_RAIN_FOREST':
+                return 0;
+            case 'TEMPERATE_DECIDUOUS_FOREST':
+                return 0;
+            case 'GRASSLAND':
+                return 0;
+            case 'TEMPERATE_DESERT':
+                return 0;
+            case 'TROPICAL_RAIN_FOREST':
+                return 0;
+            case 'TROPICAL_SEASONAL_FOREST':
+                return 0;
+            case 'GRASSLAND':
+                return 0;
+            case 'SUBTROPICAL_DESERT':
+                return 0;
+            default:
+                console.error('unknown biome: ', biome);
+                return 0;
+        }
+    };
+
     getMap() {
-        return this._map;
+        console.time('pintPoint');
+        let DATA = [];
+        for (let i = 0; i < 1000 * 1000; i++) {
+            DATA[i] = 1;
+        }
+        this._map.centers.map((center) => {
+            var mask = new PIXI.Graphics();
+            let points = center.corners.map((corner) => {
+                return corner.point;
+            });
+            mask.drawPolygon(points);
+            let bounds = mask.getBounds();
+            const x = Math.floor(bounds.x);
+            const y = Math.floor(bounds.y);
+            const biomeNr = this.mapBiomeToSpriteNr(center.biome);
+            for (let i = x; i < bounds.width + x; i++) {
+                for (let j = y; j < bounds.height + y; j++) {
+                    if (mask.graphicsData[0].shape.contains(i, j)) {
+                        DATA[i * (j + 1)] = biomeNr;
+                    }
+                }
+            }
+            mask.destroy();
+        });
+        console.timeEnd('pintPoint');
+        return DATA;
     }
 }
 
